fa8c0b1ddd5fc9b9f6899808c8f4ab7575cd416d
Daniel Alm <daniel.alm.grundstrom@protonmail.com>
2017-04-17 18:39:30 +0200

    Adds comments to the interfaces TimelineViewListener and ModelChangedListener

--- src/main/java/interfaces/ModelChangedListener.java
+++ src/main/java/interfaces/ModelChangedListener.java
@@ -2,9 +2,30 @@ package interfaces;
 
 import java.util.List;
 
+/**
+ * Interface used to listen for changes in the model class TimelineContainer.
+ * 
+ * MainController implements this interface so it can be notified whenever a timeline is added,
+ * edited or removed from TimelineContainer.
+ * 
+ * In order to listen for changes to TimelineContainer, the object implementing this interface must 
+ * first be registered as a listener using the registerListener method.
+ * 
+ * Implementing classes:
+ * 				MainController
+ * 
+ * @author Mustafa Alsaid and Daniel Alm Grunström
+ * @version 0.00.00
+ * @name ModelChangedListener.java
+ */
 public interface ModelChangedListener {
 	
-	public void onModelChanged();
+	/**
+	 * Called on the registered listener whenever a timeline is added, edited or deleted in TimelineContainer. 
+	 * 
+	 * @param timelines - The updated list of timelines
+	 */
 	//TODO:Add TimeLine Parameter.
+	public void onModelChanged();
 
 }
--- src/main/java/interfaces/TimeLineViewListener.java
+++ src/main/java/interfaces/TimeLineViewListener.java
@@ -1,12 +1,49 @@
 package interfaces;
 
+/**
+ * Interface used to listen for user interaction events (button clicks, etc.) on the timeline view.
+ * 
+ * TimelineViewController implements this interface so that TimelineView can notify the controller
+ * whenever a user clicks on the add event button, hovers the mouse over an event, clicks on the 
+ * edit event button or clicks on the delete event button.
+ * 
+ * For the implementing class to be notified, it must first be registered as a listener to TimelineView
+ * by calling the method registerListener.
+ * 
+ * Implementing classes:
+ * 				TimelineViewController
+ * 
+ * @author Mustafa Alsaid and Daniel Alm Grunström
+ * @version 0.00.00
+ * @name ModelChangedListener.java
+ */
 public interface TimeLineViewListener {
 
+	/**
+	 * Called whenever the user clicks on the add event button.
+	 */
 	public void onAddEventClicked();
 	
+	/**
+	 * Called whenever the user hovers the mouse over an event. 
+	 * 
+	 * @param name - name of the event which the user hovered over
+	 */
 	public void onMouseOverEvent(String name);
 	
+	/**
+	 * Called whenever the user clicks on the delete event button located
+	 * on the popup displayed when the user hovers over an event.
+	 * 
+	 * @param name - name of the event which the user wants to delete
+	 */
 	public void onDeleteEventClicked(String name);
 	
+	/**
+	 * Called whenever the user clicks on the edit event button located
+	 * on the popup displayed when the user hovers over an event.
+	 * 
+	 * @param name - name of the event which the user wants to edit
+	 */
 	public void onEditEventClicked(String name);
 }
