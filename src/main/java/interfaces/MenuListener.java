defbff8c01857847e7a0b191b86522e75dc3a7b5
MustafaAlsaid2022 <fancyman222@gmail.com>
2017-04-17 19:00:13 +0200

    comments for MenuListener interface

--- src/main/java/interfaces/MenuListener.java
+++ src/main/java/interfaces/MenuListener.java
@@ -1,14 +1,22 @@
 package interfaces;
 
 public interface MenuListener {
-
+	
+	/*This method is used by menu controller when the user press AddTimeLine button*/
 	public void onAddButtonClicked(); 
 	
+	/*This method is used by menu controller when the user press OpenTimeLine button
+	  to open a specific TimeLine */
 	public void onOpenButtonClicled();
 	
+	/*This method is used by menu controller when the user press DeleteTimeLine button*/
 	public void onDeleteButtonClicled();
 	
+	/*This method is used by menu controller when the user press SaveTimeLine button to save 
+	 a TimeLine*/
 	public void onSaveButtonClicled();
 	
+	/*This method is used by menu controller when the user select a specific
+	  loaded TimeLine from the loaded TiimeLine list*/
 	public void onNEWTimeLineSelected(String timeLineName);
 }
