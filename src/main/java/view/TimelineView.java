5c12ab39df67d51c2dc8c851307114dd51ca6e64
Jesper Bergström <jesper.b96@gmail.com>
2017-05-02 06:07:44 +0200

    Completely clears the TimelineView when timeline is deleted

--- src/main/java/view/TimelineView.java
+++ src/main/java/view/TimelineView.java
@@ -99,76 +99,79 @@ public class TimelineView extends StackPane {
 	public void setTimeline(Timeline timeline) {
 
 		currentTimeline = timeline;
-		addEventButton.setVisible(true);
-		drawColumns();
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
-		formatter = formatter.withLocale(Locale.ENGLISH);
-
-		// Temporary list of events.
-		ArrayList<Event> events = new ArrayList<Event>();
-		events.add(new Event(1, "one", "desc", LocalDate.parse("2017-May-04", formatter),
-				LocalDate.parse("2017-May-07", formatter), EventType.DURATION));
-		events.add(new Event(2, "two", "desc", LocalDate.parse("2017-May-05", formatter),
-				LocalDate.parse("2017-May-07", formatter), EventType.DURATION));
-		events.add(new Event(3, "three", "desc", LocalDate.parse("2017-May-06", formatter),
-				LocalDate.parse("2017-May-10", formatter), EventType.DURATION));
-		events.add(new Event(4, "four", "desc", LocalDate.parse("2017-May-08", formatter),
-				LocalDate.parse("2017-May-12", formatter), EventType.DURATION));
-		events.add(new Event(5, "five", "desc", LocalDate.parse("2017-May-07", formatter),
-				LocalDate.parse("2017-May-12", formatter), EventType.NON_DURATION));
-
-		ArrayList<EventShape> shapeList = new ArrayList<EventShape>();
-
-		int trueWidth = width + 1;
-
-		// Convert list of events in timeline to a list of EventShapes.
-		for (int i = 0; i < events.size(); i++) {
-			EventType type = events.get(i).getType();
-			int length;
-			int start = (int) (ChronoUnit.DAYS.between(timeline.getStartDate(), events.get(i).getStartDate()));
-			if (events.get(i).getType() == EventType.DURATION) {
-				length = (int) (ChronoUnit.DAYS.between(events.get(i).getStartDate(), events.get(i).getEndDate()));
-			} else {
-				length = 1;
-			}
-			EventShape shape = new EventShape(type, start * trueWidth, length * trueWidth);
-			shapeList.add(shape);
-		}
-
-		// List that holds all added events.
-		ArrayList<EventShape> added = new ArrayList<EventShape>();
-
-		// Algorithm for placing the EventShapes in the timeline view.
+		addEventButton.setVisible(false);
 		for (int i = 1; i < panes.length; i++) { // Clear the timeline view.
 			panes[i].getChildren().clear();
 		}
-
-		for (int i = 0; i < shapeList.size(); i++) {
-
-			if (i == 0) { // First event.
-				panes[1].getChildren().add(shapeList.get(i).getShape());
-				added.add(shapeList.get(i));
-			} else {
-				boolean found = false;
-				added.add(shapeList.get(i));
-
-				for (int j = 1; !found; j++) {
-					panes[j].getChildren().add(shapeList.get(i).getShape());
-
-					for (int k = 0; k < added.size() - 1; k++) {
-						if (shapeList.get(i).isOverlapping(added.get(k))
-								&& panes[j].getChildren().contains(added.get(k).getShape())) {
-							panes[j].getChildren().remove(shapeList.get(i).getShape());
-							found = false;
-							break;
+		drawColumns();
+		
+		if(currentTimeline != null){
+			addEventButton.setVisible(true);
+	
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+			formatter = formatter.withLocale(Locale.ENGLISH);
+	
+			// Temporary list of events.
+			ArrayList<Event> events = new ArrayList<Event>();
+			events.add(new Event(1, "one", "desc", LocalDate.parse("2017-May-04", formatter),
+					LocalDate.parse("2017-May-07", formatter), EventType.DURATION));
+			events.add(new Event(2, "two", "desc", LocalDate.parse("2017-May-05", formatter),
+					LocalDate.parse("2017-May-07", formatter), EventType.DURATION));
+			events.add(new Event(3, "three", "desc", LocalDate.parse("2017-May-06", formatter),
+					LocalDate.parse("2017-May-10", formatter), EventType.DURATION));
+			events.add(new Event(4, "four", "desc", LocalDate.parse("2017-May-08", formatter),
+					LocalDate.parse("2017-May-12", formatter), EventType.DURATION));
+			events.add(new Event(5, "five", "desc", LocalDate.parse("2017-May-07", formatter),
+					LocalDate.parse("2017-May-12", formatter), EventType.NON_DURATION));
+	
+			ArrayList<EventShape> shapeList = new ArrayList<EventShape>();
+	
+			int trueWidth = width + 1;
+	
+			// Convert list of events in timeline to a list of EventShapes.
+			for (int i = 0; i < events.size(); i++) {
+				EventType type = events.get(i).getType();
+				int length;
+				int start = (int) (ChronoUnit.DAYS.between(timeline.getStartDate(), events.get(i).getStartDate()));
+				if (events.get(i).getType() == EventType.DURATION) {
+					length = (int) (ChronoUnit.DAYS.between(events.get(i).getStartDate(), events.get(i).getEndDate()));
+				} else {
+					length = 1;
+				}
+				EventShape shape = new EventShape(type, start * trueWidth, length * trueWidth);
+				shapeList.add(shape);
+			}
+	
+			// List that holds all added events.
+			ArrayList<EventShape> added = new ArrayList<EventShape>();
+	
+			// Algorithm for placing the EventShapes in the timeline view.
+			for (int i = 0; i < shapeList.size(); i++) {
+	
+				if (i == 0) { // First event.
+					panes[1].getChildren().add(shapeList.get(i).getShape());
+					added.add(shapeList.get(i));
+				} else {
+					boolean found = false;
+					added.add(shapeList.get(i));
+	
+					for (int j = 1; !found; j++) {
+						panes[j].getChildren().add(shapeList.get(i).getShape());
+	
+						for (int k = 0; k < added.size() - 1; k++) {
+							if (shapeList.get(i).isOverlapping(added.get(k))
+									&& panes[j].getChildren().contains(added.get(k).getShape())) {
+								panes[j].getChildren().remove(shapeList.get(i).getShape());
+								found = false;
+								break;
+							}
+							found = true;
 						}
-						found = true;
 					}
 				}
 			}
 		}
-	}
+	}	
 
 	/**
 	 * Method that registers the listeners for the timeline view.
@@ -200,34 +203,34 @@ public class TimelineView extends StackPane {
 				days = i;
 			}
 			days++;
-		}
 
-		// Draw the columns
-		for (int i = 0; i <= days; i++) {
-
-			column = new BorderPane();
-			column.setPrefWidth(width);
-
-			rect = new Rectangle();
-			rect.setWidth(width);
-			rect.setHeight(screenSize.getHeight());
-			rect.setStroke(Color.BLACK);
-			rect.setOpacity(0.3);
-			rect.setFill(Color.WHITE);
-
-			String day = String.valueOf(currentTimeline.getStartDate().plusDays(i).getDayOfMonth());
-			text = new Text();
-			text.setFont(Font.font("Arial", 18));
-			text.setText(day);
-
-			BorderPane txtContainer = new BorderPane();
-			txtContainer.setCenter(text);
-			txtContainer.setPrefHeight(20);
-
-			column.setTop(txtContainer);
-			column.setBottom(rect);
-			dates.getChildren().add(column);
-		}
+			// Draw the columns
+			for (int i = 0; i <= days; i++) {
+	
+				column = new BorderPane();
+				column.setPrefWidth(width);
+	
+				rect = new Rectangle();
+				rect.setWidth(width);
+				rect.setHeight(screenSize.getHeight());
+				rect.setStroke(Color.BLACK);
+				rect.setOpacity(0.3);
+				rect.setFill(Color.WHITE);
+	
+				String day = String.valueOf(currentTimeline.getStartDate().plusDays(i).getDayOfMonth());
+				text = new Text();
+				text.setFont(Font.font("Arial", 18));
+				text.setText(day);
+	
+				BorderPane txtContainer = new BorderPane();
+				txtContainer.setCenter(text);
+				txtContainer.setPrefHeight(20);
+	
+				column.setTop(txtContainer);
+				column.setBottom(rect);
+				dates.getChildren().add(column);
+			}
+		}	
 	}
 	
 	/**
